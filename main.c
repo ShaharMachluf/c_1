aee7be933761d0c783c2f6594d92db584a4c0cb4
ShaharMachluf <85555432+ShaharMachluf@users.noreply.github.com>
2021-11-07 19:31:47 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include "NumClass.h"
+int main(){
+    int x,y,low,high;
+    scanf("%d %d", &x, &y);
+    if(x<y){
+        low=x;
+        high=y;
+    }
+    else{
+        low=y;
+        high=x;
+    }
+    printf("The Armstrong numbers are:");
+    for(int i=0;i<=high;i++){
+        if(i>=low&&isArmstrong(i)==1){
+            printf(" %d",i);
+        }
+    }
+    printf("\nThe Palindromes are:");
+    for(int i=low;i<=high;i++){
+        if(isPalindrome(i)==1){
+            printf(" %d",i);
+        }
+    }
+    printf("\nThe Prime numbers are:");
+    for(int i=low;i<=high;i++){
+        if(isPrime(i)==1){
+            printf(" %d",i);
+        }
+    }
+    printf("\nThe Strong numbers are:");
+    for(int i=low;i<=high;i++){
+        if(isStrong(i)==1){
+            printf(" %d",i);
+        }
+    }
+}
